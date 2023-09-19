5864727976fd3078806ecf51b84cb7260c093ce2
MałgorzataKurtek <kurtek.malgorzata@gmail.com>
2023-09-19 20:25:00 +0200

    added Category entity and CategoryRepository repository

---
+++ src/main/java/pl/coderslab/charity/model/Category.java
@@ -0,0 +1,17 @@
+package pl.coderslab.charity.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category")
+@Setter
+@Getter
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
