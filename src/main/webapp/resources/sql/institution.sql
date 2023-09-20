2cf5b830949062490fa444fef10dba3da9216c90
MałgorzataKurtek <kurtek.malgorzata@gmail.com>
2023-09-20 21:53:40 +0200

    show the list of institutions

---
+++ src/main/webapp/resources/sql/institution.sql
@@ -0,0 +1,6 @@
+INSERT INTO `charity-donation`.institution (id, description, name) VALUES (1, 'Cel i misja: Pomoc dzieciom z ubogich rodzin.', 'Fundacja "Dbam o Zdrowie"');
+INSERT INTO `charity-donation`.institution (id, description, name) VALUES (2, 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej', 'Fundacja “Dla dzieci"');
+INSERT INTO `charity-donation`.institution (id, description, name) VALUES (3, 'Cel i misja: Pomoc wybudzaniu dzieci ze śpiączki.', 'Fundacja "A kogo"');
+INSERT INTO `charity-donation`.institution (id, description, name) VALUES (4, 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.', 'Fundacja “Bez domu”');
+INSERT INTO `charity-donation`.institution (id, description, name) VALUES (5, 'Cel i misja: Opieka, wsparcie i edukacja dzieci z rodzin dotkniętych problemami społecznymi i wychowawczymi.', 'Fundacja "Pociecha"');
+INSERT INTO `charity-donation`.institution (id, description, name) VALUES (6, 'Cel i misja: Pomoc dorosłym osobom z niepełnosprawnością intelektualną', 'Fundacja "Mimo wszystko"');
